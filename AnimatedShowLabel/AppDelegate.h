6265761222b7b935e4868dc930a9a3681cc18970
Cobb <cobb.hu@livestar.com>
2016-06-22 10:18:49 +0800

    Initial Commit

---
+++ AnimatedShowLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimatedShowLabel
+//
+//  Created by Cobb on 16/6/22.
+//  Copyright © 2016年 Cobb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimatedShowLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimatedShowLabel
+//
+//  Created by Cobb on 16/6/22.
+//  Copyright © 2016年 Cobb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
